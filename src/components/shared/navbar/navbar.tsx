b89d65e3d0822cc065926131794fbb9d9c40bfd1
Elyor Shokirov <elton08@list.ru>
2025-12-16 23:45:40 +0200

    fix bugs6

--- src/components/shared/navbar/navbar.tsx
+++ src/components/shared/navbar/navbar.tsx
@@ -41,7 +41,7 @@ function Navbar() {
 								className='relative group cursor-pointer'
 								onClick={() => handleScroll(item.id)}
 							>
-								<span className='transition-colors duration-300 group-hover:text-primary'>
+								<span className='transition-colors duration-300 group-hover:text-primary text-black'>
 									{item.name}
 								</span>
 								<span className='absolute left-0 -bottom-1 h-0.5 w-0 bg-primary transition-all duration-300 group-hover:w-full'></span>
